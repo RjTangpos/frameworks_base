1f243999d7fd63acffd306dc600d9b030bbdd0ce
jhonboy121 <alfredmathew05@gmail.com>
2022-01-22 14:06:54 +0530

    base: AudioService: bail out if ringer mode is not recognized
    * So that it won't crash system server
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -5398,6 +5398,8 @@ public class AudioService extends IAudioService.Stub
                         break;
                 }
                 break;
+            default:
+                return;
         }
         maybeVibrate(effect, reason);
         setRingerModeInternal(ringerMode, reason);
