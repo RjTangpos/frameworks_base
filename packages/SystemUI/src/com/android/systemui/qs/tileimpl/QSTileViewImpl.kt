70dcbf70ebfec6e8596958c7d959d312d91296aa
Xayah <zds1249475336@gmail.com>
2022-08-19 13:46:08 -0400

    SystemUI: Reset to textColorPrimaryInverse for light theme
    * Most of them have been replaced with textColorOnAccent in b1444e4dc126357a79acb2e8e8e5e8d8578cf326, which is not in harmony with light theme.
    
    Change-Id: I72a3a33e4e4a4fc9f23458d2c0000ca4c3fbae7d
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -98,8 +98,10 @@ open class QSTileViewImpl @JvmOverloads constructor(
     private val colorInactive = Utils.getColorAttrDefaultColor(context, R.attr.offStateColor)
     private val colorUnavailable = Utils.applyAlpha(UNAVAILABLE_ALPHA, colorInactive)
 
-    private val colorLabelActive = Utils.getColorAttrDefaultColor(context, R.attr.onShadeActive)
-    private val colorLabelInactive = Utils.getColorAttrDefaultColor(context, R.attr.onShadeInactive)
+    private val colorLabelActive =
+            Utils.getColorAttrDefaultColor(context, com.android.internal.R.attr.textColorPrimaryInverse)
+    private val colorLabelInactive =
+            Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
     private val colorLabelUnavailable =
         Utils.getColorAttrDefaultColor(context, R.attr.outline)
 
